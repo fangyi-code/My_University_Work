f1c48c7969f616118e03bd32bdd8fd3a9a7780d9
fangyi-code <fangyi2004918@163.com>
2026-03-13 11:43:29 +0200

    Delete Drug Management System directory

--- Drug Management System/common/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-#.models：相对导入，指向同一目录下的 models.py
-from .models import Customer
-# Register your models here.
-#admin.site：Django 管理后台的全局注册表
-admin.site.register(Customer)
--- Drug Management System/sales/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views 
-
-urlpatterns = [
-    path('orders/',views.listorders),
-    path('customers/',views.listcustomers),
-]
